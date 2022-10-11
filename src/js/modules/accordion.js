ddef9aa69b6c281d4990cdd51350ea9a7eb871d2
Sergey <nykzak1@gmail.com>
2022-10-11 14:06:19 +0300

    Final

---
+++ src/js/modules/accordion.js
@@ -0,0 +1,26 @@
+const accordion = (triggersSelector) => {
+    const btns = document.querySelectorAll(triggersSelector);
+
+    btns.forEach(btn => {
+        btn.addEventListener('click', function() {
+
+           if (!this.classList.contains('active-style')) {
+               btns.forEach(btn => {
+                   btn.classList.remove('active-style');
+                   btn.nextElementSibling.classList.remove('active-content');
+                   btn.nextElementSibling.style.maxHeight = "0px";
+               });
+           }
+           this.classList.toggle('active-style');
+           this.nextElementSibling.classList.toggle('active-content');
+
+           if (this.classList.contains('active-style')) {
+               this.nextElementSibling.style.maxHeight = this.nextElementSibling.scrollHeight + 80 + "px";
+           } else {
+               this.nextElementSibling.style.maxHeight = "0px";
+           }
+        });
+    });
+};
+
+export default accordion;
